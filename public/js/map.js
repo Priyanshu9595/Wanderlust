11e36c2571225baaa625ce001d88309293426a3f
Delta Student <souravkr9012@gmail.com>
2024-08-22 21:51:35 +0530

    first commit

---
+++ public/js/map.js
@@ -0,0 +1,13 @@
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9, // starting zoom
+});
+
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(coordinates) // Listing.geometry.coordinates
+  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHtml("<h3>Welcome to wonderlust</h3>"))
+  .addTo(map);
